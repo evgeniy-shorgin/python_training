26f9df1eb259e9e1a169e4936e652ff9e5e6ef95
evgeniy-shorgin <evgeniy.shorgin@gmail.com>
2016-07-17 02:24:44 +0300

    Выделены методы и классы для контакта

---
+++ contact.py
@@ -0,0 +1,27 @@
+
+
+class Contact:
+    
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, company_address, telephone_home,
+                 telephone_mobile, telephone_work, telephone_fax, email, email2, email3, homepage, birthday_year,
+                 anniversary, secondary_address, secondary_phone_home, secondary_notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.company_address = company_address
+        self.telephone_home = telephone_home
+        self.telephone_mobile = telephone_mobile
+        self.telephone_work = telephone_work
+        self.telephone_fax = telephone_fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.birthday_year = birthday_year
+        self.anniversary = anniversary
+        self.secondary_address = secondary_address
+        self.secondary_phone_home = secondary_phone_home
+        self.secondary_notes = secondary_notes
