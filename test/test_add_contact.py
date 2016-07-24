fed36169f559efd8615f60851522583c22a07c6e
evgeniy-shorgin <evgeniy.shorgin@gmail.com>
2016-07-25 02:22:36 +0300

    Выделены классы для работы с группами и контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,12 +13,12 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="firstname", middlename="middlename", lastname="lastname",
-                                    nickname="nickname", title="title", company="company",
-                                    company_address="company_address", telephone_home="telephone_home",
-                                    telephone_mobile="telephone_mobile", telephone_work="telephone_work",
-                                    telephone_fax="telephone_fax", email="email", email2="email2",
-                                    email3="email3", homepage="homepage", birthday_year="birthday_year",
-                                    anniversary="anniversary", secondary_address="secondary_address",
-                                    secondary_phone_home="secondary_phone_home", secondary_notes="secondary_notes"))
+    app.contact.create(Contact(firstname="firstname", middlename="middlename", lastname="lastname",
+                               nickname="nickname", title="title", company="company",
+                               company_address="company_address", telephone_home="telephone_home",
+                               telephone_mobile="telephone_mobile", telephone_work="telephone_work",
+                               telephone_fax="telephone_fax", email="email", email2="email2",
+                               email3="email3", homepage="homepage", birthday_year="birthday_year",
+                               anniversary="anniversary", secondary_address="secondary_address",
+                               secondary_phone_home="secondary_phone_home", secondary_notes="secondary_notes"))
     app.session.logout()
